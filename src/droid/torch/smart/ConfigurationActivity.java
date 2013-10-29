54dbb533699cc906fd10bd8a21418e1361601935
Noor <nalam@appcelerator.com>
2013-10-28 19:09:19 -0700

    improvements

--- src/droid/torch/smart/ConfigurationActivity.java
+++ src/droid/torch/smart/ConfigurationActivity.java
@@ -2,12 +2,14 @@ package droid.torch.smart;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class ConfigurationActivity extends FragmentActivity
+public class ConfigurationActivity extends ActionBarActivity
 {
 
     @Override
@@ -17,18 +19,36 @@ public class ConfigurationActivity extends FragmentActivity
 	setContentView(R.layout.configuration_activity);
 	FragmentManager fragmentManager = getSupportFragmentManager();
 	FragmentTransaction ftr = fragmentManager.beginTransaction();
-	
+
 	Fragment fragment = new ConfigurationFragment();
 	ftr.add(R.id.fragment_container, fragment);
 	ftr.commit();
+	
+	getActionBar().setDisplayHomeAsUpEnabled(true);
+	//getActionBar().setTitle("");
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu)
-//    {
-//	// Inflate the menu; this adds items to the action bar if it is present.
-//	getMenuInflater().inflate(R.menu.configuration, menu);
-//	return true;
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+	getMenuInflater().inflate(R.menu.configuration, menu);
+	return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+	Toast.makeText(getApplicationContext(), "adfadfasd :: "+ item.getItemId(), Toast.LENGTH_LONG).show();
+	switch (item.getItemId())
+	{
+	case android.R.id.home:
+	    finish();
+	    return true;
+
+	default:
+	    return super.onOptionsItemSelected(item);
+	}
+        
+    }
 
 }
--- src/droid/torch/smart/ConfigurationFragment.java
+++ src/droid/torch/smart/ConfigurationFragment.java
@@ -11,7 +11,7 @@ public class ConfigurationFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-	View view = inflater.inflate(R.layout.configuration, container,false);
+	 View view = inflater.inflate(R.layout.configuration, container,false);
         return view;
     }
 }
